3b0b0ddff8b55438b0b85bdbcb2cd414d468b064
Abubakr Aqeel <abubakraqeel@gmail.com>
2025-06-24 17:35:13 +0200

    fixed app variable name

--- application.py
+++ application.py
@@ -2,9 +2,9 @@ from app import create_app
 import os
 
 
-app = create_app()
+application = create_app()
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5001))
-    app.run(debug=True, port=port, host='0.0.0.0')
+    application.run(debug=True, port=port, host='0.0.0.0')
     
